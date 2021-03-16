dde010e8d375c17bb3dd7e0a80991662a38926fc
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-16 15:11:52 -0400

    actions and reducers folder added, reducer index created with empty initState and store and provider imported into manin index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
